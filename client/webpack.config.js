631014bcbbd0717e9772e2926e47d08ab502b179
kabdomora <114969831+kabdomora@users.noreply.github.com>
2023-03-11 13:01:57 -0800

    plugins and modules added

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,53 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin'
+      }),
+      new InjectManifest({
+        swSrc: './src/sw.js',
+        swDest: 'service-worker.js',
+      }), 
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'JATE PWA - Chapter 19 Challenge',
+        short_name: 'JATE',
+        description: 'This is chapter 19 challenge',
+        background_color: '#ffffff',
+        theme_color: '#225ca3',
+        start_url:'./',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },          
+        ],
+      }),     
     ],
 
     module: {
+      // CSS loaders
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // Babel-loader
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
